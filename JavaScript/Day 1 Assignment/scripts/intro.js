62e437542a9d9bd72a8c90ae591877909954b267
Divya Kampalli <divya17032000@gmail.com>
2020-07-17 22:29:06 +0530

    Add files via upload

---
+++ JavaScript/Day 1 Assignment/scripts/intro.js
@@ -0,0 +1,16 @@
+console.log("I am javascript code");
+console.log(1);
+console.error("This is an error");
+console.warn("This is a warning");
+console.time()
+console.timeEnd();
+console.clear();
+console.table({'a':1, 'b':2}); 
+for(let i=0;i<5;i++){ 
+    console.count(i); 
+} 
+console.group('simple'); 
+console.groupEnd('simple'); 
+var number=10;
+let num=19;
+const name="Divya";
